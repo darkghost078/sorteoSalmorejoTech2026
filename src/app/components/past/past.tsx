c3ef267a0e6d46458d9d0804ba872bafc2f16db9
David19 <davidsanchezpalomino200603@gmail.com>
2026-03-03 19:14:05 +0100

    fix(components): Fixed past component centering

--- src/app/components/past/past.tsx
+++ src/app/components/past/past.tsx
@@ -13,37 +13,39 @@ export const Past = () => {
                     ¿Quieres saber más de nosotros?
                 </div>
             </div>
-            <div className={`${styles.webscontainer}`}>
-                Échale un vistazo a nuestros eventos anteriores.
-                <div className={`${styles.webs}`}>
-                    <a href="https://salmorejo.tech/2025/" target="blank" className="w-full">
-                        <Image 
-                            src={Salmorejo2025}
-                            alt="Salmorejo2025"
-                            className={`${styles.web} w-full`}
-                        ></Image>
-                    </a>
-                    <a href="https://salmorejo.tech/2024/" target="blank" className="w-full">
-                        <Image 
-                            src={Salmorejo2024}
-                            alt="Salmorejo2024"
-                            className={`${styles.web} w-full`}
-                        ></Image>
-                    </a>
-                    <a href="https://salmorejo.tech/2023/" target="blank" className="w-full">
-                        <Image 
-                            src={Salmorejo2023}
-                            alt="Salmorejo2023"
-                            className={`${styles.web} w-full`}
-                        ></Image>
-                    </a>
-                    <a href="https://salmorejo.tech/2022/" target="blank" className="w-full">
-                        <Image 
-                            src={Salmorejo2022}
-                            alt="Salmorejo2022"
-                            className={`${styles.web} w-full`}
-                        ></Image>
-                    </a>
+            <div className="grid justify-items-center">
+                <div className={`${styles.webscontainer}`}>
+                    Échale un vistazo a nuestros eventos anteriores.
+                    <div className={`${styles.webs}`}>
+                        <a href="https://salmorejo.tech/2025/" target="blank" className="w-full">
+                            <Image 
+                                src={Salmorejo2025}
+                                alt="Salmorejo2025"
+                                className={`${styles.web} w-full`}
+                            ></Image>
+                        </a>
+                        <a href="https://salmorejo.tech/2024/" target="blank" className="w-full">
+                            <Image 
+                                src={Salmorejo2024}
+                                alt="Salmorejo2024"
+                                className={`${styles.web} w-full`}
+                            ></Image>
+                        </a>
+                        <a href="https://salmorejo.tech/2023/" target="blank" className="w-full">
+                            <Image 
+                                src={Salmorejo2023}
+                                alt="Salmorejo2023"
+                                className={`${styles.web} w-full`}
+                            ></Image>
+                        </a>
+                        <a href="https://salmorejo.tech/2022/" target="blank" className="w-full">
+                            <Image 
+                                src={Salmorejo2022}
+                                alt="Salmorejo2022"
+                                className={`${styles.web} w-full`}
+                            ></Image>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
