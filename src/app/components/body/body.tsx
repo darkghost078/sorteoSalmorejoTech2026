0aa360003f408d5333fbcf37300834f72ede0fde
David Martinez Molina <daviddarkghost@gmail.com>
2026-03-16 17:37:31 +0100

    Frankenstein done

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -10,14 +10,14 @@ export default function body() {
           LA CITA DE LA TECNOLOGÍA CORDOBESA REGRESA CON UNA ENERGÍA IMPARABLE
         </h1>
         <h1 className={styles.text2}>8 DE MAYO DE 2026</h1>
-        <a
+        {/*<a
           href="https://docs.google.com/forms/d/e/1FAIpQLSdNOP1mYrdZi2WQhaiZ-nMbRqYowmwW1T4RYTBlrnShJ-ukxA/viewform"
           target="_blank"
           rel="noopener noreferrer"
           className={styles.miboton}
         >
           CALL4PAPER
-        </a>
+        </a>*/}
 
 
       </div>
