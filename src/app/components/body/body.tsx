698c2514a5cc9fb2a1d313280fa2b8725a7bec75
ByteKata08 <melladoarenasjuan@gmail.com>
2026-04-02 16:45:38 +0200

    update: Entradas button done.

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -4,22 +4,21 @@ export default function body() {
   return (
     <div className={styles.container}>
       <div className={styles.fondo}>
-        <div className={styles.image}>
-        </div>
+        <div className={styles.image}></div>
         <h1 className={styles.text}>
           LA CITA DE LA TECNOLOGÍA CORDOBESA REGRESA CON UNA ENERGÍA IMPARABLE
         </h1>
         <h1 className={styles.text2}>8 DE MAYO DE 2026</h1>
-        {/*<a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSdNOP1mYrdZi2WQhaiZ-nMbRqYowmwW1T4RYTBlrnShJ-ukxA/viewform"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.miboton}
-        >
-          CALL4PAPER
-        </a>*/}
-
-
+        {
+          <a
+            href="https://www.eventbrite.es/e/entradas-salmorejotech-2026-1986054851524"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.miboton}
+          >
+            ENTRADAS
+          </a>
+        }
       </div>
     </div>
   );
