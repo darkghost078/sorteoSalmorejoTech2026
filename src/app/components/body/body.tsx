6fb4a369203f191d6e330799adc3a8505c698d34
david martinez <daviddarkghost@gmail.com>
2026-04-24 11:30:55 +0200

    init

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -2,24 +2,8 @@ import styles from "./body.module.css";
 
 export default function body() {
   return (
-    <div className={styles.container}>
-      <div className={styles.fondo}>
-        <div className={styles.image}></div>
-        <h1 className={styles.text}>
-          LA CITA DE LA TECNOLOGÍA CORDOBESA REGRESA CON UNA ENERGÍA IMPARABLE
-        </h1>
-        <h1 className={styles.text2}>8 DE MAYO DE 2026</h1>
-        {
-          <a
-            href="https://www.eventbrite.es/e/entradas-salmorejotech-2026-1986054851524"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.miboton}
-          >
-            ENTRADAS
-          </a>
-        }
-      </div>
+    <div>
+      HOLA
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,13 @@
 "use client";
-import FindUs from "@/app/components/findUs/findUs";
-import Navbar from "@/app/components/navbar/navbar";
 import Body from "@/app/components/body/body";
 import Footer from "@/app/components/footer/footer";
-import Timetable from "@/app/components/Timetable/Timetable";
-import Cards from "./components/Cards/Cards";
-import Description from "@/app/components/description/description";
 
-import { motion } from "framer-motion";
-import { useState } from "react";
-import { SponsorProps } from "../app/components/Sponsors";
-import sponsors from "@/data/sponsors.json";
-import { SponsorsGrid } from "@/app/components/Sponsors/grid";
 
 export default function Home() {
-  const [isAnimationComplete, setIsAnimationComplete] = useState(false);
   return (
     <div className="relative" style={{ minHeight: "100vh" }}>
-      <motion.div
-        className="relative z-10 flex flex-col flex-grow"
-        initial="hidden"
-        animate="visible"
-        onAnimationComplete={() => setIsAnimationComplete(true)}
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-            y: 50,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.5,
-              duration: 1.25,
-            },
-            y: 0,
-          },
-        }}
-        style={{ position: "relative", zIndex: 10 }}
-      >
-        <Navbar />
-        <Body />
-        <Description />
-        <FindUs />
-        <Timetable />
-        <Cards/>
-        <SponsorsGrid sponsors={sponsors as SponsorProps[]} />
-        <Footer isVisible={isAnimationComplete} />
-      </motion.div>
+      <Body/>
+      <Footer isVisible={true} />
     </div>
   );
 }
